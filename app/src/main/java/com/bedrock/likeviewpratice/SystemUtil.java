ebb028c178b7d0b55013b0794a5f045809b3ad59
bladeofgod <450103187@qq.com>
2019-01-08 09:28:35 +0800

    first commit

---
+++ app/src/main/java/com/bedrock/likeviewpratice/SystemUtil.java
@@ -0,0 +1,34 @@
+package com.bedrock.likeviewpratice;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.TypedValue;
+
+public class SystemUtil {
+
+    /**
+     * dp转px
+     *
+     * @param context
+     * @param dpVal
+     * @return
+     */
+    public static int dp2px(Context context, float dpVal){
+        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dpVal,context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * sp转px
+     * @param context
+     * @param spVal
+     * @return
+     *
+     */
+
+    public static int sp2px(Context context,float spVal){
+        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,spVal,context.getResources().getDisplayMetrics());
+    }
+
+
+
+}
